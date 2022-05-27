a0115e1b9b931302207c9f48bc417ecc5eda28b6
stockwell <stockywell@gmail.com>
2022-05-27 13:49:43 +1000

    Updater: Get local IP so you don't have to hardcode it

--- tools/updater.py
+++ tools/updater.py
@@ -4,15 +4,27 @@ import asyncio
 import requests
 import uuid
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(('1.1.1.1', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 async def initiate():
-    url = "http://COFFEE.local/api/v1/update/initiate"
+	url = "http://COFFEE.local/api/v1/update/initiate"
 
-    request = {
-            'URL'  : 'http://192.168.1.240:8070/ESPresso.bin',
-            'UUID' : str(uuid.uuid4())
-    }
+	request = {
+		'URL'  : 'http://' + str(get_ip()) + ':8070/ESPresso.bin',
+		'UUID' : str(uuid.uuid4())
+	}
 
-    x = requests.post(url, json = request)
+	x = requests.post(url, json = request, timeout = 0.01)
     
 loop = asyncio.get_event_loop()
 try:

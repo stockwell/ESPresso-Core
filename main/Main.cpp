1bc199807f01841ae40f367c572bf1b6075ec4e8
stockwell <stockywell@gmail.com>
2022-05-30 23:03:31 +1000

    Main: Init Wifi after everything else to ensure that SSR for boiler isn't left on

--- main/Main.cpp
+++ main/Main.cpp
@@ -23,9 +23,6 @@ static void mainTask(void* pvParameter)
 	ESP_ERROR_CHECK(esp_netif_init());
 	ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-	Wifi::InitWifi(Wifi::WifiMode::STA);
-	Wifi::InitMDNS();
-
 	auto boilerEventLoop = std::make_unique<BoilerEventLoop>();
 	auto temperatureEventLoop = std::make_unique<TemperatureEventLoop>(boilerEventLoop.get());
 	auto pressureEventLoop = std::make_unique<PressureEventLoop>();
@@ -33,6 +30,9 @@ static void mainTask(void* pvParameter)
 
 	boilerEventLoop->setTemperature(BoilerEventLoop::BrewTargetTemp, 93.1f);
 	boilerEventLoop->setTemperature(BoilerEventLoop::SteamTargetTemp, 140.0f);
+
+	Wifi::InitWifi(Wifi::WifiMode::STA);
+	Wifi::InitMDNS();
 
 	while (1)
 		vTaskDelay(portMAX_DELAY);
